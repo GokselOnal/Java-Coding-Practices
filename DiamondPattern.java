05bcfdf93d6736f4de91f750868c259372036c52
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-13 13:32:22 +0300

    print a numeric diamond pattern using nested loops

---
+++ DiamondPattern.java
@@ -0,0 +1,24 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1 ; j <= -1*i + 5; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1 ; k <= 2 * i -1; k++ ){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        for(int i = 4 ; i >= 1 ; i--){
+            for(int j = 1 ; j <= -1 * i + 5 ; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1 ; k <= 2 * i -1 ; k++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}

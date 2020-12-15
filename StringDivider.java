e0b692142350ccf155413be0a625f9b659d0448c
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-15 09:01:30 +0300

    Divide strings and concatanate the chars

---
+++ StringDivider.java
@@ -0,0 +1,20 @@
+public class StringDivider {
+
+    public static String divide(String str1, String str2){
+        System.out.print(str1 +"\t");
+        System.out.print(str2 + "\t");
+
+        String str3 = "";
+        for(int i = 0 ; i < str1.length() ; i++){
+            for(int j = 0 ; j < str2.length(); j++){
+                str3 += "" + str1.charAt(i) + str2.charAt(j) + " " ;
+            }
+        }
+        return str3;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(divide("ABCD","EFGH"));
+    }
+}

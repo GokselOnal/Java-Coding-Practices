b3606ed7a5cd929022ee9c82eb2870e68a33981c
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-19 21:59:30 +0300

    check the given string is palindrome or not

---
+++ Palindrome.java
@@ -0,0 +1,11 @@
+public class Palindrome {
+    public static boolean isPalindrome(String str){
+
+        for(int i = 0; i < str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length() - i -1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
